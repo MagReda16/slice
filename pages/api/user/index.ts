a01bbb8f8b84dc3957e11ad11d33d377bf61d0b2
Ian Tang <ian.tang@protonmail.com>
2022-01-31 18:38:14 -0500

    implement api/user route with GET and PUT methods

---
+++ pages/api/user/index.ts
@@ -0,0 +1,41 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nc from 'next-connect';
+import { errorHandler } from '../../../lib/server';
+import { User } from '../../../db/models/';
+import connectToDb from '../../../db';
+
+const userHandler = nc(errorHandler);
+
+userHandler.get(
+  async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+    await connectToDb();
+
+    try {
+      const user = await User.findById(req.body._id);
+      if (!user) throw new Error('User cannot be found');
+      res.status(200).json(user);
+    } catch (error: any) {
+      console.error(error);
+      res.status(404).json({ error });
+    }
+  }
+);
+
+userHandler.put(
+  async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+    await connectToDb();
+
+    try {
+      const updatedUser = await User.findByIdAndUpdate(req.body._id, req.body, {
+        new: true,
+      });
+      if (!updatedUser) throw new Error('User cannot be found');
+      res.status(200).json(updatedUser);
+    } catch (error: any) {
+      console.error(error);
+      res.status(404).json({ error });
+    }
+  }
+);
+
+export default userHandler;
