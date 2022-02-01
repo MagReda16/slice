2f4527a79206122c8c14b18ecee743833800ac48
Phillip Nguy <nguyphillip@gmail.com>
2022-02-01 10:07:45 -0800

    Some css added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,38 @@
 import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
+import stylesBtn from '../styles/Button.module.css';
 import Head from 'next/head';
 import Link from 'next/link';
 import NavButton from '../components/NavButton';
 
 const Home = () => {
   return (
-    <div>
-      <Head>
-        <title>Slice</title>
-      </Head>
-      <h2>Meal planning & budgeting</h2>
-      <Link href='/emailLogin' passHref>
-        <NavButton
-        className={styles.loginWithEmailBtn}
-        type='button'
-        children='Login with email'
-        /> 
-      </Link>
-      <Link href='/register'>New here? Register</Link>
-    </div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.slice}>Slice</h1>
+      <h4>Meal planning & budgeting</h4>
 
-export default Home; 
+      <div className={styles.loginContainer}>
+        <Link href='/oAuthLogin' passHref>
+          <NavButton
+            className={stylesBtn.loginWithOauthBtn}
+            type='button'
+            children='Login with Gmail'
+          />
+        </Link>
+        <Link href='/emailLogin' passHref>
+          <NavButton
+            className={stylesBtn.loginWithEmailBtn}
+            type='button'
+            children='Login with email'
+          />
+        </Link>
+      </div>
+
+      <Link href='/register'>
+        <button className={stylesBtn.regBtn}>New here? Register</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
