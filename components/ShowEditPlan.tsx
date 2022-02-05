4c0b9a18369592e8ad7356e8e8855d19de8a5e44
Ian Tang <ian.tang@protonmail.com>
2022-02-04 20:07:55 -0500

    improve organization in Forms and Button css modules, add css for edit plan page

--- components/ShowEditPlan.tsx
+++ components/ShowEditPlan.tsx
@@ -1,39 +1,40 @@
-import { MouseEventHandler } from "react";
-import DoughnutChart from "./DoughnutChart";
-import styles from '../styles/Buttons.module.css';
-import stylesContainer from '../styles/Containers.module.css'
+import { MouseEventHandler } from 'react';
+import DoughnutChart from './DoughnutChart';
 import Link from 'next/link';
-
 import RecipeList from './RecipeList';
 import { Recipe } from '../db/types';
 import NavButton from '../components/NavButton';
-
+import styles from '../styles/EditPlan.module.css';
+import buttonStyles from '../styles/Buttons.module.css';
+import containerStyles from '../styles/Containers.module.css';
 
 type ShowEditPlanProps = {
-  toggleSearch: MouseEventHandler,
-  recipes: Recipe[],
-}
+  toggleSearch: MouseEventHandler;
+  recipes: Recipe[];
+};
 
 const ShowEditPlan = ({ recipes, toggleSearch }: ShowEditPlanProps) => {
-
-
-
   return (
-    <div className={stylesContainer.container}>
+    <div className={containerStyles.container}>
       <h1>Edit my Plan</h1>
       <DoughnutChart />
-      <button className={styles.Btn} type='button' onClick={toggleSearch}>Add recipe</button>
+      <button
+        className={`${buttonStyles.btn} ${buttonStyles.small} ${styles.btn}`}
+        type="button"
+        onClick={toggleSearch}
+      >
+        Add recipe
+      </button>
       <RecipeList recipes={recipes} btnType={'subtract'} />
-      <Link href='/user/plan' passHref>
+      <Link href="/user/plan" passHref>
         <NavButton
-          className={styles.confirmBtn}
-          type='button'
-          children='Confirm'
+          className={`${buttonStyles.btn} ${buttonStyles.small} ${styles.btn}`}
+          type="button"
+          children="Confirm"
         />
       </Link>
-
     </div>
-  )
-}
+  );
+};
 
 export default ShowEditPlan;
--- components/ShowSearch.tsx
+++ components/ShowSearch.tsx
@@ -1,30 +1,43 @@
 import { MouseEventHandler, ChangeEventHandler, FormEventHandler } from 'react';
-import RecipeList from "./RecipeList";
-import { Recipe } from "../db/types";
-
+import RecipeList from './RecipeList';
+import { Recipe } from '../db/types';
+import styles from '../styles/EditPlan.module.css';
 import stylesBtn from '../styles/Buttons.module.css';
 import stylesForm from '../styles/Forms.module.css';
-import backArrow from '../assets/back-arrow-navigation-svgrepo-com.svg';
 
 type ShowSearchProps = {
-  toggleSearch: MouseEventHandler,
-  data: Recipe[],
-  changeQuery: ChangeEventHandler,
-  querySearch: string,
-  submitSearch: FormEventHandler
-}
+  toggleSearch: MouseEventHandler;
+  data: Recipe[];
+  changeQuery: ChangeEventHandler;
+  querySearch: string;
+  submitSearch: FormEventHandler;
+};
 
-const ShowSearch = ({ toggleSearch, data, changeQuery, querySearch, submitSearch }: ShowSearchProps) => {
+const ShowSearch = ({
+  toggleSearch,
+  data,
+  changeQuery,
+  querySearch,
+  submitSearch,
+}: ShowSearchProps) => {
   return (
-    <>
-      <button onClick={toggleSearch} className={stylesBtn.backArrowBtn}>
-        ⬅
-      </button>
-      <form className={stylesForm.searchbar} onSubmit={submitSearch}>
-        <input className={stylesForm.searchInput} type="text" placeholder='search...' value={querySearch} onChange={changeQuery} />
-      </form>
-      <RecipeList btnType={'add'} recipes={data}/>
-    </>
+    <main>
+      <div className={styles.searchBarContainer}>
+        <button onClick={toggleSearch} className={stylesBtn.backArrowBtn}>
+          ⬅
+        </button>
+        <form className={stylesForm.searchbar} onSubmit={submitSearch}>
+          <input
+            className={stylesForm.searchInput}
+            type="text"
+            placeholder="search..."
+            value={querySearch}
+            onChange={changeQuery}
+          />
+        </form>
+      </div>
+      <RecipeList btnType={'add'} recipes={data} />
+    </main>
   );
 };
 
