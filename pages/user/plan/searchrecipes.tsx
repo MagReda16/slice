5a05111441a4610c05d3cdd84bcbd1ba657c1dae
Ian Tang <ian.tang@protonmail.com>
2022-02-05 11:59:40 -0500

    split user/plan/edit page by adding user/plan/searchrecipes, delete ShowSearch and ShowEditPlan components and move logic into corresponding pages

---
+++ pages/user/plan/searchrecipes.tsx
@@ -0,0 +1,61 @@
+import {
+  useState,
+  ChangeEventHandler,
+  FormEventHandler,
+  ChangeEvent,
+  FormEvent,
+} from 'react';
+import RecipeList from '../../../components/RecipeList';
+import { usePlan, useSearch } from '../../../lib/hooks';
+import styles from '../../../styles/EditPlan.module.css';
+import formStyles from '../../../styles/Forms.module.css';
+import buttonStyles from '../../../styles/Buttons.module.css';
+import { useRouter } from 'next/router';
+
+const SearchRecipes = () => {
+  const router = useRouter();
+
+  const [querySearch, setQuerySearch] = useState('');
+  const [readyToSubmit, setReadyToSubmit] = useState(false);
+  const { data, searchError } = useSearch(querySearch, readyToSubmit);
+
+  if (searchError) return <div>Error or something, idk</div>;
+
+  const submitSearch: FormEventHandler = async (e: FormEvent) => {
+    e.preventDefault();
+    setReadyToSubmit(true);
+  };
+
+  const changeQuery: ChangeEventHandler = (e: ChangeEvent) => {
+    const { value } = e.target as typeof e.target & {
+      value: string;
+    };
+    setReadyToSubmit(false);
+    setQuerySearch(value);
+  };
+
+  return (
+    <main>
+      <div className={styles.searchBarContainer}>
+        <button
+          onClick={() => router.push('/user/plan/edit')}
+          className={buttonStyles.backArrowBtn}
+        >
+          ⬅
+        </button>
+        <form className={formStyles.searchbar} onSubmit={submitSearch}>
+          <input
+            className={formStyles.searchInput}
+            type="text"
+            placeholder="search..."
+            value={querySearch}
+            onChange={changeQuery}
+          />
+        </form>
+      </div>
+      <RecipeList btnType={'add'} recipes={data} />
+    </main>
+  );
+};
+
+export default SearchRecipes;
