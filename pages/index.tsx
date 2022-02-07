64dfac210d1fcaee7d3aa29cc3dea5a9f2ac8add
Phillip Nguy <nguyphillip@gmail.com>
2022-02-07 15:03:26 -0800

    has user now

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,24 @@ import stylesBtn from '../styles/Buttons.module.css';
 import Head from 'next/head';
 import Link from 'next/link';
 import NavButton from '../components/NavButton';
+import { useUser } from '@auth0/nextjs-auth0';
 
 const Home = () => {
+  const { user, error, isLoading } = useUser();
+  if (isLoading) return <div>Loading...</div>
+  if(error) return <div>{error.message}</div>
+  console.log('THIS IS USER 🥺', user)
   return (
     <div className={styles.container}>
       <header className={styles.mainHeader}>
+        <h1>{user && user.email}</h1>
         <h1>SLICE</h1>
         <h4>Meal planning & budgeting</h4>
       </header>
       <div className={styles.mainLoginContainer}>
-        <Link href='/oAuthLogin' passHref>
+      <a href="/api/auth/logout">Logout</a>
+      <a href="/api/auth/login">Login</a>
+        <Link href='/api/auth/login' passHref>
           <NavButton
             className={`${stylesBtn.btn} ${stylesBtn.login}`}
             type='button'
