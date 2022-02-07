f5719033776bb65d91beda3264f684266948c367
Phillip Nguy <nguyphillip@gmail.com>
2022-02-07 09:40:56 -0800

    css finish

--- pages/recipes/[id].tsx
+++ pages/recipes/[id].tsx
@@ -13,24 +13,33 @@ const RecipeDetails = () => {
 
   return (
     <div className={styles.container}>
-       <button onClick={() => {router.back()}} className={stylesBtn.backArrowBtn}>
-        ⬅
-      </button>
-      <h1>{data.title}</h1>
+      <div>
+        <button onClick={() => {router.back()}} className={stylesBtn.backArrowBtn}>
+          ⬅
+        </button>
+        <h1 className={styles.title}>{data.title}</h1>
+      </div>
 
+      <img className={styles.recipeImg} src={data.image} />
 
-      <img src={data.image} />
+      <div className={styles.recipeInfoContainer}>
+        <div className={styles.infoContainer}>
+          <h3 className={styles.header}>Summary</h3>
+          <p dangerouslySetInnerHTML={{ __html: data.summary }}></p>
+        </div>
 
+        <div className={styles.infoContainer}>
+          <h3 className={styles.header}>Ingredients</h3>
+          <ul>
+            {data.extendedIngredients.map((ingredient: any) => <li key={ingredient.id}>{ingredient.original}</li>)}
+          </ul>
+        </div>
 
-      <p dangerouslySetInnerHTML={{ __html: data.summary }}></p>
-
-      <h3>Ingredients</h3>
-      <ul>
-        {data.extendedIngredients.map((ingredient: any) => <li key={ingredient.id}>{ingredient.original}</li>)}
-      </ul>
-
-      <h3>Instructions</h3>
-      <div dangerouslySetInnerHTML={{ __html: data.instructions }}></div>
+        <div className={styles.infoContainer}>
+          <h3 className={styles.header}>Instructions</h3>
+          <div dangerouslySetInnerHTML={{ __html: data.instructions }}></div>
+        </div>
+      </div>
     </div>
   );
 };
