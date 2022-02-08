e00475cf16c7ada00a54b86418ce9afc1a2e3e3e
edakhmetov <78399160+edakhmetov@users.noreply.github.com>
2022-02-07 20:11:12 -0500

    updated env in next config

--- lib/clients/backendApi.ts
+++ lib/clients/backendApi.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const backendApiClient = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: process.env.baseApiUrl,
   headers: {
     'Content-Type': 'application/json',
-    // contentType: 'application/json'
   }
 })
 
